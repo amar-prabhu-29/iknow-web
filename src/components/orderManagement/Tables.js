e11be77aed45bbc31e432fa9616226d928883021
Amar Prabhu <4nm16cs051@nmamit.in>
2019-07-21 20:49:48 +0530

    Added Order Details Display

--- src/components/orderManagement/Tables.js
+++ src/components/orderManagement/Tables.js
@@ -4,16 +4,25 @@ class Tables extends Component {
     state = {
         currentTable: null
     }
+    setActiveTable = (table) => {
+        this.setState({
+            currentTable: table  //Replace activeHallTables[0] with parameter table after implementation
+        })
+        
+    }
     render() {
         let activeHallTables = this.props.tables
         .filter(table => table.hall === this.props.hall)
         .map(table => {
-                return (table);    {/* Style This Line For Tables inside the return statement  */}
+            return (table);    {/* Style This Line For Tables inside the return statement  */}
         })
+        this.setActiveTable(activeHallTables[0]);   {/* Remove this line after Implementation of Click Table to change State */}
         return (
             <div>
-                {activeHallTables} 
-                <Orders table={this.state.currentTable} /> {/* state variable currentTable must change whenever a table is clicked or selected.  */}
+                {/* Variable activHallTables displayed Here*/}
+                <Orders table={this.state.currentTable} /> 
+                {/* state variable currentTable must change whenever a table is clicked or selected. 
+                USE METHOD setActiveTable AS IMPLEMENTED ON LINE 19 */}
             </div>
         )
     }
